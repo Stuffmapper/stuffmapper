f9e55ce5ac92293f3738d41a6ddadb3a4618084c
RyantheFarmer <ryan.the.farmer@gmail.com>
2016-05-14 22:23:03 -0700

    fixed up that there masonry and fixed the toggles switch
    see title

--- js/app.js
+++ js/app.js
@@ -4,99 +4,105 @@ var stuffMapp = angular
 .controller("MainController", MainController)
 .config(appConfig)
 .factory('$userData', function() {
-    var data = { FirstName: '', LastName: '', email: '', userId: 0 };
-    return {
-        getFirstName: function () { return data.FirstName; },
-        setFirstName: function (firstName) { data.FirstName = firstName; },
-        getLastName: function () { return data.LastName; },
-        setLastName: function (lastName) { data.LastName = lastName; },
-        getUserId: function () { return data.UserId; },
-        setUserId: function (userId) { data.UserId = userId; },
-        getEmail: function () { return data.Email; },
-        setEmail: function (email) { data.Email = email; },
-        clearData: function () {
-            data = { FirstName: '', LastName: '', email: '', userId: 0 };
-        }
-    };
+	var data = { FirstName: '', LastName: '', email: '', userId: 0 };
+	return {
+		getFirstName: function () { return data.FirstName; },
+		setFirstName: function (firstName) { data.FirstName = firstName; },
+		getLastName: function () { return data.LastName; },
+		setLastName: function (lastName) { data.LastName = lastName; },
+		getUserId: function () { return data.UserId; },
+		setUserId: function (userId) { data.UserId = userId; },
+		getEmail: function () { return data.Email; },
+		setEmail: function (email) { data.Email = email; },
+		clearData: function () {
+			data = { FirstName: '', LastName: '', email: '', userId: 0 };
+		}
+	};
+})
+.directive('repeatDone', function() {
+	return function(scope, element, attrs) {
+		if(scope.$last) scope.$eval(attrs.repeatDone);
+	};
 });
 
+
 if(config.ionic.isIonic) {
-    stuffMapp.run(function($ionicPlatform) {
-        $ionicPlatform.ready(function() {
-            if(window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                cordova.plugins.Keyboard.disableScroll(true);
-            }
-            if(window.StatusBar) {
-                StatusBar.styleDefault();
-            }
-        });
-    });
+	stuffMapp.run(function($ionicPlatform) {
+		$ionicPlatform.ready(function() {
+			if(window.cordova && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+				cordova.plugins.Keyboard.disableScroll(true);
+			}
+			if(window.StatusBar) {
+				StatusBar.styleDefault();
+			}
+		});
+	});
 }
 
 function appConfig($locationProvider, $stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/stuff/get');
-    $locationProvider.html5Mode(config.html5);
-    $stateProvider
-    .state('stuff', {
-        url: '/stuff',
-        templateUrl: 'templates/partial-home.html',
-        controller: function($scope, $userData) {
-            $scope.map = new google.maps.Map($('#map-view')[0], {
-                center: {
-                    lat: 47.608013,
-                    lng: -122.335167
-                },
-                zoom: 13
-            });
-            $userData.setUserId(1);
-        }
-    })
-    .state('stuff.get', {
-        url: '/get',
-        templateUrl: 'templates/partial-home-getstuff.html',
-        controller: GetStuffController
-    })
-    .state('stuff.get.item', { //  http://www.stuffmapper.com/stuff/get/232
-        url: '/:id',
-        controller: GetItemController
-    })
-    .state('stuff.give', { //  http://www.stuffmapper.com/stuff/give#step1
-        url: '/give',
-        templateUrl: 'templates/partial-home-givestuff.html',
-        controller: GiveStuffController
-    })
-    .state('stuff.my', {
-        url: '/my',
-        templateUrl: 'templates/partial-home-mystuff.html',
-        controller: MyStuffController
-    })
-    .state('stuff.my.item', {
-        url: '/:id',
-        controller: MyStuffController
-    })
-    .state('about', {
-    });
+	$urlRouterProvider.otherwise('/stuff/get');
+	$locationProvider.html5Mode(config.html5);
+	$stateProvider
+	.state('stuff', {
+		url: '/stuff',
+		templateUrl: 'templates/partial-home.html',
+		controller: function($scope, $userData) {
+			$scope.map = new google.maps.Map($('#map-view')[0], {
+				center: {
+					lat: 47.608013,
+					lng: -122.335167
+				},
+				zoom: 13
+			});
+			$userData.setUserId(1);
+		}
+	})
+	.state('stuff.get', {
+		url: '/get',
+		templateUrl: 'templates/partial-home-getstuff.html',
+		controller: GetStuffController
+	})
+	.state('stuff.get.item', { //  http://www.stuffmapper.com/stuff/get/232
+		url: '/:id',
+		controller: GetItemController
+	})
+	.state('stuff.give', { //  http://www.stuffmapper.com/stuff/give#step1
+		url: '/give',
+		templateUrl: 'templates/partial-home-givestuff.html',
+		controller: GiveStuffController
+	})
+	.state('stuff.my', {
+		url: '/my',
+		templateUrl: 'templates/partial-home-mystuff.html',
+		controller: MyStuffController
+	})
+	.state('stuff.my.item', {
+		url: '/:id',
+		controller: MyStuffController
+	})
+	.state('about', {
+	});
 }
 
 function setDefaultConfig() {
-    var isIonic = $('html').hasClass('ionic');
-    var isDev = $('html').hasClass('dev') || $('html').hasClass('test');
-    var modules = ['ui.router', 'ngAnimate'];
-    if(isIonic) modules.push('ionic');
-    return {
-        ionic : {
-            isIonic : isIonic
-        },
-        modules : modules,
-        api : {
-            host : isIonic?
-            isDev?
-            '//localhost/':
-            /*'//www.stuffmapper.com/'*/'//localhost:3000/':
-            '/',
-            version : 'v1'
-        },
-        html5 : !!window.history && !!window.history.pushState
-    };
+	var isIonic = $('html').hasClass('ionic');
+	var isDev = $('html').hasClass('dev') || $('html').hasClass('test');
+	var modules = ['ui.router', 'ngAnimate'];
+	if(isIonic) modules.push('ionic');
+	return {
+		ionic : {
+			isIonic : isIonic
+		},
+		modules : modules,
+		api : {
+			host : isIonic?
+			isDev?
+			'//localhost/':
+			/*'//www.stuffmapper.com/'*/'//localhost:3000/':
+			'/',
+			version : 'v1'
+		},
+		html5 : !!window.history && !!window.history.pushState
+	};
 }
--- js/controllers/get_stuff.js
+++ js/controllers/get_stuff.js
@@ -1,97 +1,127 @@
 function GetStuffController($scope, $http, $timeout, $userData) {
-    $('#tab-container .tabs .get-stuff-tab a').addClass('selected');
-    $http({
-        method: 'GET',
-        url: config.api.host + 'api/' + config.api.version + '/stuff/'
-    }).then(function(data) {
-        $scope.listItems = data.data;
-        $scope.markers = [];
-        $scope.infowindows = [];
-        $scope.filterOptions = [
-            'thing',
-            'stuff'
-        ];
-        $scope.filterPaneOpen = false;
-        $scope.toggleFilterPane = function() {
-            $('#filter-pane').toggleClass('open-filter-pane');
-        };
+	$('#tab-container .tabs .get-stuff-tab a').addClass('selected');
+	$http({
+		method: 'GET',
+		url: config.api.host + 'api/' + config.api.version + '/stuff/'
+	}).then(function(data) {
+		$scope.listItems = data.data;
+		$scope.markers = [];
+		$scope.infowindows = [];
+		$scope.filterOptions = [
+			'thing',
+			'stuff'
+		];
+		$scope.filterPaneOpen = false;
 
-        $scope.toggleSwitch = function() {
-            $('.toggle-button').toggleClass('toggle-button-selected');
-        };
+		$scope.toggleFilterPane = function() {
+			$('#filter-pane').toggleClass('open-filter-pane');
+		};
 
-        $scope.toggleMap = function() {
-          $('#map-view').toggle().css('z-index', '2');
-        };
+		$scope.toggleSwitch = function() {
+			$('.toggle-button').toggleClass('toggle-button-selected');
+		};
 
-        $scope.mason = function() {
-          $window.onload(function() {
-            $('.masonry-grid').masonry({
-             columnWidth: 400,
-             itemSelector: '.masonry-grid-item'
-            }).imagesLoaded(function() {
-             $('.masonry-grid').masonry('reload');
-            });
-          });
-        };
+		var mapAnimationTimeout;
+		var mapIsOpen = true;
+		$scope.toggleMap = function() {
+			if(mapAnimationTimeout) clearTimeout(mapAnimationTimeout);
+			if(mapIsOpen) {
+				requestAnimationFrame(function() {
+					$('#masonry-container').removeClass('hide-masonry-container');
+					$('#map-view').removeClass('map-view-open');
+				});
+			}
+			else {
+				mapAnimationTimeout = setTimeout(function() {
+					requestAnimationFrame(function() {
+						$('#map-view').addClass('map-view-open');
+					});
+				}, 250);
+				requestAnimationFrame(function() {
+					$('#masonry-container').addClass('hide-masonry-container');
+				});
+			}
+			mapIsOpen = !mapIsOpen;
+		};
 
-        var tempSearchText = '';
-        var searchTextTimeout;
-        var lastSearch;
-        $scope.$watch('searchStuff', function (val) {
-            if (searchTextTimeout) {
-                $timeout.cancel(searchTextTimeout);
-            }
-            else {
-                console.log('Search for everything');
-            }
-            tempSearchText = val;
-            searchTextTimeout = $timeout(function() {
-                if(tempSearchText) {
-                    $scope.filterText = tempSearchText;
-                    if(tempSearchText !== lastSearch) {
-                        lastSearch = tempSearchText;
-                    }
-                }
-                else if(tempSearchText !== undefined) {
-                    console.log('Search for everything');
-                }
-            }, 250);
-        });
-        $scope.listItems.forEach(function(e) {
-            $scope.markers.push(new google.maps.Marker({
-                position: {
-                    lat: e.lat,
-                    lng : e.lng
-                },
-                icon: '/img/circle.png',
-                map: $scope.map
-            }));
-            var contentString = [
-                '<div style="width: 180px; height: 300px;">',
-                '   <div style="font-size: 16px">'+e.title+'</div>',
-                '   <div style="background-image: url('+e.img+');position:absolute;width: 200px; height: 270px;top:25px"></div>',
-                '</div>'
-            ].join('\n');
-            $scope.infowindows.push(new google.maps.InfoWindow({
-                content: contentString
-            }));
-            var infowindow = $scope.infowindows[$scope.infowindows.length - 1];
-            $scope.markers[$scope.markers.length - 1].addListener('click', function(event) {
-                $scope.infowindows.forEach(function(e) {
-                    e.close();
-                });
-                infowindow.open($scope.map, this);
-            });
-        });
-        $scope.$on("$destroy", function() {
-            $('#tab-container .tabs .get-stuff-tab a').removeClass('selected');
-            $scope.infowindows.forEach(function(e) {
-                e.close();
-            });
-            $scope.markers.forEach(function(e) {
-                e.setMap(null);
-            });
-        });
-    });
+		$scope.initMasonry = function() {
+			$('.masonry-grid').masonry({
+				columnWidth: function(columnWidth) {
+					return $('.masonry-grid').width()/2;
+				}(),
+				itemSelector: '.masonry-grid-item',
+				isAnimated: true
+			}).imagesLoaded(function(){
+				$('.masonry-grid').masonry('reloadItems').masonry();
+			});
+			$(window).resize(function() {
+				$('.masonry-grid').masonry({
+					columnWidth: function(columnWidth) {
+						return $('.masonry-grid').width()/2;
+					}(),
+					itemSelector: '.masonry-grid-item',
+					isAnimated: true
+				});
+			});
+		};
+
+		var tempSearchText = '';
+		var searchTextTimeout;
+		var lastSearch;
+		$scope.$watch('searchStuff', function (val) {
+			if (searchTextTimeout) {
+				$timeout.cancel(searchTextTimeout);
+			}
+			else {
+				console.log('Search for everything');
+			}
+			tempSearchText = val;
+			searchTextTimeout = $timeout(function() {
+				if(tempSearchText) {
+					$scope.filterText = tempSearchText;
+					if(tempSearchText !== lastSearch) {
+						lastSearch = tempSearchText;
+					}
+				}
+				else if(tempSearchText !== undefined) {
+					console.log('Search for everything');
+				}
+			}, 250);
+		});
+		$scope.listItems.forEach(function(e) {
+			$scope.markers.push(new google.maps.Marker({
+				position: {
+					lat: e.lat,
+					lng : e.lng
+				},
+				icon: '/img/circle.png',
+				map: $scope.map
+			}));
+			var contentString = [
+				'<div style="width: 180px; height: 300px;">',
+				'   <div style="font-size: 16px">'+e.title+'</div>',
+				'   <div style="background-image: url('+e.img+');position:absolute;width: 200px; height: 270px;top:25px"></div>',
+				'</div>'
+			].join('\n');
+			$scope.infowindows.push(new google.maps.InfoWindow({
+				content: contentString
+			}));
+			var infowindow = $scope.infowindows[$scope.infowindows.length - 1];
+			$scope.markers[$scope.markers.length - 1].addListener('click', function(event) {
+				$scope.infowindows.forEach(function(e) {
+					e.close();
+				});
+				infowindow.open($scope.map, this);
+			});
+		});
+		$scope.$on("$destroy", function() {
+			$('#tab-container .tabs .get-stuff-tab a').removeClass('selected');
+			$scope.infowindows.forEach(function(e) {
+				e.close();
+			});
+			$scope.markers.forEach(function(e) {
+				e.setMap(null);
+			});
+		});
+	});
 }
