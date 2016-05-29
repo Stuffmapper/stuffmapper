2375fb0ef356efdf4d46c9baf65a9f1340e1c0c6
Jack Sneed <JackSSS@users.noreply.github.com>
2016-05-29 11:42:50 -0700

    watchlist update

--- js/controllers/watchlist.js
+++ js/controllers/watchlist.js
@@ -1,6 +1,24 @@
-function WatchlistController($scope) {
-    $('#mystuff a').addClass('selected');
-    $scope.$on("$destroy", function() {
-        $('#mystuff a').removeClass('selected');
-    });
+function WatchListController($scope, $location, authentication) {
+	if(!authenticated.res.loggedIn) {
+		$location.path('stuff/get');
+		return;
+	}
+	// $('#mystuff a').addClass('selected');
+	// $scope.$on("$destroy", function() {
+	// 		$('#mystuff a').removeClass('selected');
+	// });
+	$scope.tagnames = [
+                    { 'tagname':'desk'},
+                    { 'tagname':'couch'},
+	                  { 'tagname':'table'},
+		                { 'tagname':'books'},
+			              { 'tagname':'electronics'},
+                    ];
+	$scope.addRow = function(){
+		$scope.tagnames.push({ 'tagname':$scope.tagname});
+		$scope.tagname='';
+	};
+	$scope.submitTagName = function() {
+
+	};
 }
