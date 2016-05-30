108e605776aeb8558aa6292dc62a30b531da3bda
JackSSS <sneed.jack@gmail.com>
2016-05-30 04:36:18 -0700

    add tag names to watchlist

--- js/controllers/watchlist.js
+++ js/controllers/watchlist.js
@@ -3,6 +3,9 @@ function WatchListController($scope, $location, authenticated) {
 		$location.path('stuff/get');
 		return;
 	}
+	// $scope.tagnames = [];
+	// $scope.errors = [];
+	// $scope.newTagname = null;
 	// $('#mystuff a').addClass('selected');
 	// $scope.$on("$destroy", function() {
 	// 		$('#mystuff a').removeClass('selected');
@@ -24,8 +27,50 @@ function WatchListController($scope, $location, authenticated) {
 		                {'tagname':'misc'},
 			              {'tagname':'electronics'}
                     ];
-	$scope.addRow = function(){
+	$scope.addTag = function(){
 		$scope.tagnames.push({ 'tagname':$scope.tagname});
 		$scope.tagname='';
 	};
+
+	// $scope.getAll = function() {
+	// $http.get('/api/tagname')
+	// 	.then(function(res) {
+	// 		$scope.tagnames = res.data;
+	// 	}, function(err) {
+	// 		console.log(err.data);
+	// 	});
+	// };
+	//
+	// $scope.create = function(tagname) {
+	// 	$http.post('/api/tagnames', tagname)
+	// 		.then(function(res) {
+	// 			$scope.tagnames.push(res.data);
+	// 			$scope.newTagName = null;
+	// 		}, function(err) {
+	// 			console.log(err.data);
+	// 		});
+	// };
+	//
+	// $scope.update = function(tagname) {
+	// 	tagname.editing = false;
+	// 	$http.put('/api/tagname/' + tagname._id, tagname)
+	// 		.then(function(res) {
+	// 			console.log('this tagname has a been modified');
+	// 		}, function(err) {
+	// 			$scope.errors.push('could not get tagname: ' + tagname.name);
+	// 			console.log(err.data);
+	// 		});
+	// };
+	//
+	// $scope.remove = function(tagname) {
+	// 	$scope.tagnames.splice($scope.tagnames.indexOf(tagname), 1);
+	// 	$http.delete('/api/tagname/' + tagname._id)
+	// 		.then(function(res) {
+	// 			console.log('tagname deleted');
+	// 		}, function(err) {
+	// 			console.log(err.data);
+	// 			$scope.errors.push('could not delete tagname: ' + tagname.name);
+	// 			$scope.getAll();
+	// 		});
+	// };
 }
