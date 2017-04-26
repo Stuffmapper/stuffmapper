b358e2ecd2293c8f97b65ded87fd7644d6ea45c1
mabc224 <mabc224@gmail.com>
2017-04-26 22:56:40 +0500

    modify user setting page #10

--- src/js/controllers/settings.js
+++ src/js/controllers/settings.js
@@ -5,6 +5,7 @@ function SettingsController() {
 	var $http = arguments[1];
 	var authenticator = arguments[2];
 	var $state = arguments[3];
+
 	$http.post(config.api.host + '/api/v' + config.api.version + '/account/status?nocache='+new Date().getTime()).success(function(data){
 		if(!data.res.user) {
 			$state.go('stuff.get', {'#':'signin'});
@@ -23,9 +24,40 @@ function SettingsController() {
 				}
 			});
 			// Editing user data
-
 			$scope.update = function(users) {
 
+				var emailRe = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+				$('#setting-phone, #setting-uname, #setting-email').css({border:''});
+				$('#setting-error-warning-container').html('');
+				var valid = true;
+				var message = '';
+				var formSetting = {
+					email: $('#setting-email').val().toLowerCase(),
+					uname: $('#setting-uname').val(),
+					phone: $('#setting-phone').val()
+				};
+				if(!formSetting.uname) {valid=false;$('#setting-uname').css({border:'1px solid red'});message='please insert a username name';}
+				if(!formSetting.phone) {valid=false;$('#setting-phone').css({border:'1px solid red'});message='please insert a phone name';}
+				if(!formSetting.email || !emailRe.test(formSetting.email)) {valid=false;$('#setting-email').css({border:'1px solid red'});message='invalid email address';}
+
+				if(!valid) {
+					$('#setting-error-warning-container').html('<div class="sm-full-width sm-negative-warning">'+message+'</div>');
+					return;
+				} else {
+					if(!$scope.userSettings.uname.$valid){
+						$('#setting-error-warning-container').html('<div class="sm-full-width sm-negative-warning">Uname is not valid</div>');
+						return;
+					}
+					if(!$scope.userSettings.phone.$valid){
+						$('#setting-error-warning-container').html('<div class="sm-full-width sm-negative-warning">Phone # is not valid</div>');
+						return;
+					}
+					if(!$scope.userSettings.email.$valid){
+						$('#setting-error-warning-container').html('<div class="sm-full-width sm-negative-warning">Email is not valid</div>');
+						return;
+					}
+				}		
+				$scope.users.phone_number = $scope.users.phone_number.replace(/\s+/g, '');
 				$http.put('/api/v1/account/info', $scope.users,{
 					headers: {
 						'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
