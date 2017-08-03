01c74bff14f004b2c7999a753ad5485719f6d749
mabc224 <mabc224@gmail.com>
2017-08-03 22:05:22 +0500

    info window feature

--- src/js/controllers/stuff.js
+++ src/js/controllers/stuff.js
@@ -25,13 +25,13 @@ function StuffController() {
 	var styledMap = new google.maps.StyledMapType(style[0], {name: "Styled Map"});
 	var zoomLevel;
 	var mapOptions = {
-		zoom: 13,
+		zoom: 12,
 		center: {
 			lat: 47.608013,
 			lng: -122.335167
 		},
 		minZoom: 10,
-		maxZoom: 16,
+		maxZoom: 17,
 		zoomControl: false,
 		mapTypeControl: false,
 		scaleControl: false,
@@ -57,7 +57,7 @@ function StuffController() {
 			if(zoomLevel-1 >= 10) $scope.map.setZoom(--zoomLevel);
 		});
 		$('#sm-map-zoom-in').click(function() {
-			if(zoomLevel+1 <= 16) $scope.map.setZoom(++zoomLevel);
+			if(zoomLevel+1 <= 17) $scope.map.setZoom(++zoomLevel);
 		});
 	},1200);
 }
